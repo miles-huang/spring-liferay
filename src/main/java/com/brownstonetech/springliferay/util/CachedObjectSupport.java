6ce839e60dad49c0030f9b9585e1b81b400411a7
Miles Huang <miles.huang@live.com>
2016-03-22 16:56:47 -0400

    Captcha support

--- src/main/java/com/brownstonetech/springliferay/util/CachedObjectSupport.java
+++ src/main/java/com/brownstonetech/springliferay/util/CachedObjectSupport.java
@@ -3,14 +3,19 @@ package com.brownstonetech.springliferay.util;
 public abstract class CachedObjectSupport implements ExpirationValidateable {
 
 	private long lastExpirationValidationTime = 0L;
+	private long expireCheckInterval;
 	private static final long EXPIRATION_CHECK_INTERVAL = 5000;
+	
+	public CachedObjectSupport(long expireCheckInterval) {
+		this.expireCheckInterval = expireCheckInterval;
+	}
 	
 	@Override
 	public boolean isExpired() {
 		long current = System.currentTimeMillis();
 		long lastChecked = lastExpirationValidationTime;
 		lastExpirationValidationTime = current;
-		if ( current - lastChecked > EXPIRATION_CHECK_INTERVAL) {
+		if ( current - lastChecked > expireCheckInterval) {
 			return doExpirationCheck();
 		}
 		return false;
