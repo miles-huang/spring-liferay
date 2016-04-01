6509627f4cc0b0ff94027638c39ca582f52a61e2
Miles Huang <miles.huang@live.com>
2016-04-01 17:07:36 -0400

    Add FieldConstantsExt

---
+++ src/main/java/com/brownstonetech/springliferay/util/dynamicdatalist/FieldConstantsExt.java
@@ -0,0 +1,10 @@
+package com.brownstonetech.springliferay.util.dynamicdatalist;
+
+import com.liferay.portlet.dynamicdatamapping.storage.FieldConstants;
+
+public class FieldConstantsExt extends FieldConstants {
+
+	public static final String LOCALIZED = "localized";
+	
+	public static final String CLASS_NAME = "className";
+}
